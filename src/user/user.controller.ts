e68d23c412f921dd83becc36d30f103afc8addda
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2023-07-23 10:28:52 -0300

    primeiro commit

---
+++ src/user/user.controller.ts
@@ -0,0 +1,57 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common'
+import { CreateUserDTO } from './dto/create-user.dto'
+
+@Controller('users')
+export class UserController {
+  @Post()
+  async create(@Body() body: CreateUserDTO) {
+    return { ...body }
+  }
+
+  @Get()
+  async list() {
+    return { users: [] }
+  }
+
+  @Get(':id')
+  async show(@Param() params) {
+    return {
+      user: {},
+      params,
+    }
+  }
+
+  @Put(':id')
+  async update(@Body() body, @Param() params) {
+    return {
+      method: 'put',
+      body,
+      params,
+    }
+  }
+
+  @Patch(':id')
+  async updatePartial(@Body() body, @Param() params) {
+    return {
+      method: 'patch',
+      body,
+      params,
+    }
+  }
+
+  @Delete(':id')
+  async delete(@Param() params) {
+    return {
+      params,
+    }
+  }
+}
